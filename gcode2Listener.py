45eedc59205c34b8f90476e7e8e99bf58aa9139f
alessandroBrugneraSilva <alessandrobrugnera.s@gmail.com>
2020-05-07 13:07:25 -0300

    feito. Modificaoes no listener enter foram feitas.

--- gcode2Listener.py
+++ gcode2Listener.py
@@ -1,102 +1,104 @@
 # Generated from gcode2.g4 by ANTLR 4.8
 from antlr4 import *
+
 if __name__ is not None and "." in __name__:
     from .gcode2Parser import gcode2Parser
 else:
     from gcode2Parser import gcode2Parser
 
+
 # This class defines a complete listener for a parse tree produced by gcode2Parser.
 class gcode2Listener(ParseTreeListener):
 
     # Enter a parse tree produced by gcode2Parser#prg.
-    def enterPrg(self, ctx:gcode2Parser.PrgContext):
+    def enterPrg(self, ctx: gcode2Parser.PrgContext):
         pass
 
     # Exit a parse tree produced by gcode2Parser#prg.
-    def exitPrg(self, ctx:gcode2Parser.PrgContext):
+    def exitPrg(self, ctx: gcode2Parser.PrgContext):
         pass
 
-
     # Enter a parse tree produced by gcode2Parser#fimprograma.
-    def enterFimprograma(self, ctx:gcode2Parser.FimprogramaContext):
+    def enterFimprograma(self, ctx: gcode2Parser.FimprogramaContext):
         pass
 
     # Exit a parse tree produced by gcode2Parser#fimprograma.
-    def exitFimprograma(self, ctx:gcode2Parser.FimprogramaContext):
+    def exitFimprograma(self, ctx: gcode2Parser.FimprogramaContext):
         pass
 
-
     # Enter a parse tree produced by gcode2Parser#statement.
-    def enterStatement(self, ctx:gcode2Parser.StatementContext):
+    def enterStatement(self, ctx: gcode2Parser.StatementContext):
         pass
 
     # Exit a parse tree produced by gcode2Parser#statement.
-    def exitStatement(self, ctx:gcode2Parser.StatementContext):
+    def exitStatement(self, ctx: gcode2Parser.StatementContext):
         pass
 
-
     # Enter a parse tree produced by gcode2Parser#numerolinha.
-    def enterNumerolinha(self, ctx:gcode2Parser.NumerolinhaContext):
+    def enterNumerolinha(self, ctx: gcode2Parser.NumerolinhaContext):
         pass
 
     # Exit a parse tree produced by gcode2Parser#numerolinha.
-    def exitNumerolinha(self, ctx:gcode2Parser.NumerolinhaContext):
+    def exitNumerolinha(self, ctx: gcode2Parser.NumerolinhaContext):
         pass
 
-
     # Enter a parse tree produced by gcode2Parser#mfim.
-    def enterMfim(self, ctx:gcode2Parser.MfimContext):
+    def enterMfim(self, ctx: gcode2Parser.MfimContext):
         pass
 
     # Exit a parse tree produced by gcode2Parser#mfim.
-    def exitMfim(self, ctx:gcode2Parser.MfimContext):
+    def exitMfim(self, ctx: gcode2Parser.MfimContext):
         pass
 
-
     # Enter a parse tree produced by gcode2Parser#mfunc.
-    def enterMfunc(self, ctx:gcode2Parser.MfuncContext):
+    def enterMfunc(self, ctx: gcode2Parser.MfuncContext):
         pass
 
     # Exit a parse tree produced by gcode2Parser#mfunc.
-    def exitMfunc(self, ctx:gcode2Parser.MfuncContext):
+    def exitMfunc(self, ctx: gcode2Parser.MfuncContext):
         pass
 
-
     # Enter a parse tree produced by gcode2Parser#codfunc.
-    def enterCodfunc(self, ctx:gcode2Parser.CodfuncContext):
+    def enterCodfunc(self, ctx: gcode2Parser.CodfuncContext):
         pass
 
     # Exit a parse tree produced by gcode2Parser#codfunc.
-    def exitCodfunc(self, ctx:gcode2Parser.CodfuncContext):
+    def exitCodfunc(self, ctx: gcode2Parser.CodfuncContext):
         pass
 
-
     # Enter a parse tree produced by gcode2Parser#coordx.
-    def enterCoordx(self, ctx:gcode2Parser.CoordxContext):
+    def enterCoordx(self, ctx: gcode2Parser.CoordxContext):
+        coordenada = ""
+        for element in ctx.children[1:]:
+            coordenada += str(element)
+
+        print(coordenada + " ", end = '')
         pass
 
     # Exit a parse tree produced by gcode2Parser#coordx.
-    def exitCoordx(self, ctx:gcode2Parser.CoordxContext):
+    def exitCoordx(self, ctx: gcode2Parser.CoordxContext):
         pass
 
-
     # Enter a parse tree produced by gcode2Parser#coordy.
-    def enterCoordy(self, ctx:gcode2Parser.CoordyContext):
+    def enterCoordy(self, ctx: gcode2Parser.CoordyContext):
+        coordenada = ""
+        for element in ctx.children[1:]:
+            coordenada += str(element)
+
+        print(coordenada + " ")
         pass
 
     # Exit a parse tree produced by gcode2Parser#coordy.
-    def exitCoordy(self, ctx:gcode2Parser.CoordyContext):
+    def exitCoordy(self, ctx: gcode2Parser.CoordyContext):
         pass
 
-
     # Enter a parse tree produced by gcode2Parser#fimdelinha.
-    def enterFimdelinha(self, ctx:gcode2Parser.FimdelinhaContext):
+    def enterFimdelinha(self, ctx: gcode2Parser.FimdelinhaContext):
         pass
 
     # Exit a parse tree produced by gcode2Parser#fimdelinha.
-    def exitFimdelinha(self, ctx:gcode2Parser.FimdelinhaContext):
+    def exitFimdelinha(self, ctx: gcode2Parser.FimdelinhaContext):
         pass
-
 
 
 del gcode2Parser
--- main.py
+++ main.py
@@ -2,15 +2,22 @@ import sys
 from antlr4 import *
 from gcode2Lexer import gcode2Lexer
 from gcode2Parser import gcode2Parser
+from gcode2Listener import gcode2Listener
 
 
 def main(argv):
-    input_stream = FileStream(argv[1])
+    input_stream = FileStream(argv)
     lexer = gcode2Lexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = gcode2Parser(stream)
-    tree = parser.startRule()
+    tree = parser.prg()
 
+    listener = gcode2Listener()
+
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+    # print(tree.toStringTree(recog=parser))
 
 if __name__ == '__main__':
-    main(sys.argv)
+    # main(sys.argv)
+    main("input.txt")
